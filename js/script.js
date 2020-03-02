49e9fea446477a692aa8f8ac31a60ffc5bdf40ae
Andwhit022 <59718154+Andwhit022@users.noreply.github.com>
2020-03-02 02:15:19 -0500

    created global variables

--- js/script.js
+++ js/script.js
@@ -16,8 +16,8 @@ FSJS project 2 - List Filter and Pagination
    will only be used inside of a function, then it can be locally 
    scoped to that function.
 ***/
-
-
+let studentList = document.querySelector('.student-list');
+let pageItems = 0;
 
 
 /*** 
