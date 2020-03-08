3b4b3c634daed29d1d8b4e55207d04b067423526
Andwhit022 <59718154+Andwhit022@users.noreply.github.com>
2020-03-07 23:21:16 -0500

    update appendPageLinks function and clean code

--- js/script.js
+++ js/script.js
@@ -6,37 +6,17 @@ FSJS project 2 - List Filter and Pagination
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-let studentList = document.querySelectorAll("student-item cf");
+
+let studentList = document.querySelectorAll('li.student-item');
 let pageItems = 10;
-let page = document.querySelector(".page");
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
 
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
+
+   //Create the `showPage` function to hide all of the items in the 
+   
 const showPage  = (list, pageNum) => {
    let startIndex = (pageNum * pageItems) - pageItems;
-   let endIndex = pageNum * pageItems;
+   let endIndex = (pageNum * pageItems)-1;
 
    for (let i =0; i < studentList.length; i ++){
       if (i >= startIndex && i <= endIndex ){
@@ -51,27 +31,49 @@ showPage(studentList, 1);
 
 
 
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
+
+   //Create the `appendPageLinks function` to generate, append, and add 
+  // functionality to the pagination buttons.
+
 
 const appendPageLinks = (list) => {
-   let maxPages = studentList.length/pageItems;
+   let maxPages = Math.ceil(studentList.length/pageItems);
+
    let linkDiv = document.createElement('div');
    linkDiv.className = 'pagination';
+
+   let page = document.querySelector('.page');
    page.appendChild(linkDiv);
+
    let ul = document.createElement('ul');
-   ul.appendChild(linkDiv);
+   linkDiv.appendChild(ul);
+
 
    for(let i = 0; i < maxPages; i ++){
       let li =document.createElement('li');
-      li.appendChild(ul);
+      li.value= i+1;
+      ul.appendChild(li);
+
       let a = document.createElement('a');
-      a.appendChild(li);
-      a[0].className = 'active'
+      li.appendChild(a);
+
+      a.textContent= i+1;
+      a.className = '';
       a.href = '#';
-      let pageButton = document.createElement(button);
+      a.addEventListener('click', ()=> {
+         let pageButton = event.target;
+         pageButton.className = 'active';
+
+         let PageNumberButton = a.textContent;
+         
+         let pageLink = document.getElementsByTagName('a');
+
+         for(let l=0; l < pageLink.length; l++){
+            pageLink[l].className = '';
+         }
+         showPage(studentList, PageNumberButton);
+      });
+      
    }
 }
 
