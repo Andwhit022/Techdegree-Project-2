055291feb63977320af168840f63cd395f614145
Andwhit022 <59718154+Andwhit022@users.noreply.github.com>
2020-03-09 21:59:32 -0400

    update variables and clean code

--- js/script.js
+++ js/script.js
@@ -5,15 +5,15 @@ FSJS project 2 - List Filter and Pagination
    
 
 //set global variables to hold student list items and number of items on each page.
-let studentList = document.querySelectorAll('li.student-item');
-let pageItems = 10;
+const studentList = document.querySelectorAll('li.student-item');
+const pageItems = 10;
 
 
 //Create the `showPage` function to hide all of the items in the list except the ten on each page.   
 const showPage  = (list, pageNum) => {
    //set starIndex and endIndex variables to determine what interval of students to show
-   let startIndex = (pageNum * pageItems) - pageItems;
-   let endIndex = (pageNum * pageItems)-1;
+   const startIndex = (pageNum * pageItems) - pageItems;
+   const endIndex = (pageNum * pageItems)-1;
 
    //for any list greater than or equal to start index And less than end index print the list item
    for (let i =0; i < studentList.length; i ++){
@@ -34,27 +34,27 @@ showPage(studentList, 1);
 **/
 const appendPageLinks = (list) => {
    //set a variable to hold the number of pages needed
-   let maxPages = Math.ceil(studentList.length/pageItems);
+   const maxPages = Math.ceil(list.length/pageItems);
 
    //create and append a div with the class name 'pagination'
-   let linkDiv = document.createElement('div');
+   const linkDiv = document.createElement('div');
    linkDiv.className = 'pagination';
 
    //set page variable to hold page div and append pagination div to page div
-   let page = document.querySelector('.page');
+   const page = document.querySelector('.page');
    page.appendChild(linkDiv);
 
    //create a ul element and append it to pagination div
-   let ul = document.createElement('ul');
+   const ul = document.createElement('ul');
    linkDiv.appendChild(ul);
 
    //for every page create a li and 'a' element
    for(let i = 0; i < maxPages; i ++){
-      let li =document.createElement('li');
+      const li =document.createElement('li');
       li.value= i+1;
       ul.appendChild(li);
 
-      let a = document.createElement('a');
+      const a = document.createElement('a');
       li.appendChild(a);
 
       //set each 'a' elements text content to hold its page number
@@ -65,20 +65,21 @@ const appendPageLinks = (list) => {
       //add event listener to each 'a' element
       a.addEventListener('click', ()=> {
 
-         //set the clicked button's class name to 'active'
-         let pageButton = event.target;
-         pageButton.className = 'active';
-
-         let PageNumberButton = a.textContent;
+         const PageNumberButton = a.textContent;
          
-         let pageLink = document.getElementsByTagName('a');
+         const pageLink = document.getElementsByTagName('a');
 
          //remove to the 'active' class name from other 'a' elements
          for(let l=0; l < pageLink.length; l++){
             pageLink[l].classList.remove('active');
          }
+
+         //set the clicked button's class name to 'active'
+         const pageButton = event.target;
+         pageButton.className = 'active';
+         
          //call the show page function and pass the student list and PageNumberButton
-         showPage(studentList, PageNumberButton);
+         showPage(list, PageNumberButton);
       });
    }
 }
